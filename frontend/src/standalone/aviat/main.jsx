9244d8e7f3a2b587d206bb72969e6fd305f43fc1
Mazen ElAli <mazenelali69@gmail.com>
2026-03-13 13:07:06 +0200

    Initial local snapshot

---
+++ frontend/src/standalone/aviat/main.jsx
@@ -0,0 +1,10 @@
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+import AviatStandalonePage from "./AviatStandalonePage";
+import "./aviat-standalone.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AviatStandalonePage />
+  </React.StrictMode>
+);
